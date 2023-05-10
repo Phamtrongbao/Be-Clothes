f93853d565bb54d42c5235a05ec4ae1a87c517c6
trongbao123 <phambao11062001>
2023-05-10 13:44:33 +0700

    cập nhật

--- src/Controller/BrandController.js
+++ src/Controller/BrandController.js
@@ -2,17 +2,32 @@ const Brand = require("../models/Brand");
 
 //[GET] /Brand
 const GetBrand = async (req, res, next) => {
-  Brand.find().then((course) => {
-      console.log("cour", course);
-      res.status(200).json(course);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).send({
-        message: "Lỗi khi tìm kiếm các brand",
-        error: error.message,
-      });
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  try {
+    const brands = await Brand.find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort({ createdAt: -1 })
+      .exec();
+
+    const count = await Brand.countDocuments().exec();
+
+    const totalPages = Math.ceil(count / limit);
+
+    res.status(200).json({
+      brands: brands,
+      currentPage: page,
+      totalPages: totalPages,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Lỗi khi tìm kiếm các brand",
+      error: error.message,
+    });
+  }
 };
 
 //[Get] /Brand/:id/Details
